2613b23cc34b04ff1710d791b8cd45a62331fb02
xkmato <kenneth@uvotam.com>
2024-09-25 11:02:10 +0300

    add location data

--- models/contacts/patient.sql
+++ models/contacts/patient.sql
@@ -20,9 +20,13 @@ SELECT
   person.vmmc_no,
   person.enrollment_facility,
   person.enrollment_location,
+  location.implementing_partner,
+  location.district,
+  location.province,
   couchdb.doc->>'patient_id' as patient_id
 FROM {{ ref('person') }} person
-INNER JOIN {{ env_var('POSTGRES_SCHEMA') }}.{{ env_var('POSTGRES_TABLE') }} couchdb ON couchdb._id = uuid
+JOIN {{ source('couchdb', env_var('POSTGRES_TABLE')) }} couchdb ON couchdb._id = uuid
+JOIN {{ ref('location') }} location ON location.facility ILIKE person.enrollment_facility
 WHERE couchdb.doc->>'patient_id' IS NOT NULL
 {% if is_incremental() %}
   AND person.saved_timestamp >= {{ max_existing_timestamp('saved_timestamp') }}
